6c515fa6caa8e4161618b8a86b99f1afb3ff4aec
Maksym <maxlearncode@outlook.com>
2018-02-23 19:33:01 +0100

    Added axios & ant design, components (searchbar and card)

---
+++ app/src/components/card.jsx
@@ -0,0 +1,65 @@
+import React, {Component} from 'react';
+import {Card, Icon, Avatar, Tooltip, Alert} from 'antd';
+const {Meta} = Card;
+
+class UserCard extends Component {
+  render() {
+    const user = this.props.user;
+
+    if(user.error) {
+      return (
+      <Alert
+      style={{ width: 300}}
+      message={"Error 422 :("}
+      description={user.message}
+      type="error"
+      closable
+    />
+      )
+    }
+
+    const stream = this.props.stream;
+
+    if(!Object.keys(stream).length) {
+      return (
+        <div>Loading...</div>
+      );
+    }
+
+    const userStream = this.props.stream.data.stream;
+    const banner = user.profile_banner;
+    const defaultBanner = "https://edge.alluremedia.com.au/m/k/2014/01/gaming-twitch-tv-logo.jpg";
+    const url = user.url;
+    const logo = user.logo;
+    const name = user.display_name;
+    const status = user.status;
+    const isLive = userStream ? true : false;
+
+    return (
+      <Card style={{ width: 300}}
+        cover={<img
+          alt = "banner"
+          src = {banner ? banner : defaultBanner} />}
+        actions={[
+          <Tooltip placement="bottom" title="visit channel">
+            <Icon
+              type="play-circle"
+              onClick={() => window.open(url, "_blank")}
+            />
+           </Tooltip>,
+           <Tooltip placement="bottom" title={isLive ? userStream.game : "channel offline"}>
+             <Icon type={isLive ? "check" : "close"}/>
+           </Tooltip>,
+        ]}>
+      <Meta avatar={
+        <Avatar
+          src = {logo} />}
+          title={name}
+          description={status}
+        />
+      </Card>
+    )
+  }
+}
+
+export default UserCard;
